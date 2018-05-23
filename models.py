ab5125573d3309ef1edd611e9897978cc3d3af0a
Jake <jakemacnaughton@gmail.com>
2018-05-23 17:38:33 -0400

    removed solutions

--- models.py
+++ models.py
@@ -4,28 +4,13 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
-class Actor(Base):
-    __tablename__ = 'actors'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    roles = relationship(
-        'Role',
-        secondary='actor_roles'
-    )
+# Write your classes below
 
-class Role(Base):
-    __tablename__ = 'roles'
-    id = Column(Integer, primary_key=True)
-    character = Column(String)
-    actors = relationship(
-        'Actor',
-        secondary='actor_roles'
-    )
 
-class ActorRole(Base):
-    __tablename__ = 'actor_roles'
-    actor_id = Column(Integer, ForeignKey('actors.id'), primary_key=True)
-    role_id = Column(Integer, ForeignKey('roles.id'), primary_key=True)
+
+
+
+
 
 
 engine = create_engine('sqlite:///:memory:')
--- queries.py
+++ queries.py
@@ -1,13 +1,10 @@
 from models import *
 
 def return_christian_bales_roles(session):
-    bale = session.query(Actor).filter_by(name='Christian Bale').one()
-    return bale.roles
+    # Return a list of Christian Bale role instances
 
 def return_catwoman_actors(session):
-    catwoman = session.query(Role).filter_by(character='Catwoman').one()
-    return catwoman.actors
+    #  Return a list of actor instances that have played Catwoman
 
 def return_number_of_batman_actors(session):
-    batman = session.query(Role).filter_by(character="Batman").one()
-    return len(batman.actors)
+    # Return the number of actors that have played Batman
